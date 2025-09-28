926067df78a31f6fb4e92502f2e96674890f3bc9
Dxnqa <deandreydomingo@gmail.com>
2025-09-28 14:24:11 -0800

    Responsive resize, Database init, Form fixes

--- index.js
+++ index.js
@@ -4,22 +4,46 @@ import axios from "axios";
 import { readdir, readFileSync, writeFileSync, unlink, existsSync } from "fs";
 import { fileURLToPath } from "url";
 import path from "path";
+import pg from "pg";
 
 const app = express();
 const port = 3050;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+const client = new pg.Client({
+  user: "postgres",
+  password: "<entrance0>",
+  host: "localhost",
+  port: 5432,
+  database: "pfdcvm",
+});
+
+client.connect();
+
 app.set("views", "./views");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("home.ejs", { technician: "Deandrey Domingo" });
 });
 
-app.post("/submit", (req, res) => {
-  const submission = req.body;
+app.post("/submit", async (req, res) => {
+  const submission = {
+    category: req.body["category"],
+    resolution: req.body["resolution"],
+    notes: req.body["notes"],
+  };
+  //   try {
+  //     client.query(
+  //       "INSERT INTO call_log(id, name, category, resolution, notes) VALUES ($1, $2, $3, $4, $5)",
+  //       []
+  //     );
+  //   } catch (error) {
+  //     console.error(`Error processing: ${error.stack}`);
+  //   }
+  console.log(`Submission success:`, submission);
   res.redirect("/");
-  console.log("Submission success");
 });
 
 app.listen(port, (err) => {
